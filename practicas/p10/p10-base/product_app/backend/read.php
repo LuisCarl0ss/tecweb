03f44a62b58294f3bba770cd68bb30497e431ff5
LuisCarl0ss <torralbacerezoluiscarlos@gmail.com>
2025-03-04 09:10:06 -0600

    Se creó la carpeta p10 y dentro de ella estan los archivos de la carpeta p10-base.zip

---
+++ practicas/p10/p10-base/product_app/backend/read.php
@@ -0,0 +1,30 @@
+<?php
+    include_once __DIR__.'/database.php';
+
+    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    $data = array();
+    // SE VERIFICA HABER RECIBIDO EL DATO A BUSCAR
+    if( isset($_POST['search']) ) {
+        $search = $_POST['search'];
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        $query = "SELECT * FROM productos WHERE nombre LIKE '%{$search}%' OR marca LIKE '%{$search}%' OR detalles LIKE '%{$search}%'";
+        if ( $result = $conexion->query($query) ) {
+            // SE OBTIENEN LOS RESULTADOS
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                $product = array();
+                foreach($row as $key => $value) {
+                    $product[$key] = $value; // utf8_encode($value);
+                }
+                $data[] = $product;
+            }
+            $result->free();
+        } else {
+            die('Query Error: '.mysqli_error($conexion));
+        }
+        $conexion->close();
+    } 
+    
+    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+    echo json_encode($data, JSON_PRETTY_PRINT);
+?>
